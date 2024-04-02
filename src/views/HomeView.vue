578e626f74374b6d375fc17cd70844a593b011d2
chihiro kikuchi <c-kikuchi@hi.u-tokyo.ac.jp>
2024-04-02 18:23:53 +0900

    some fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup>
 import {RouterLink} from "vue-router"
 import metalist from "../metalist.js"
+
+const isDev = import.meta.env.MODE
+
 </script>
 
 <template>
   <main>
-    <h1><img src="/ii-icon_256.png" style="width:64px; height:64px; vertical-align: bottom;">Home</h1>
+    <h1><img src="/ii-icon_256.png" style="width:64px; height:64px; vertical-align: bottom;">ii Annote Home</h1>
     <nav>
       <div v-for="meta in metalist.list">
         <RouterLink :to="'/viewer/'+meta.bookid">{{ meta.title }}</RouterLink>
@@ -14,5 +17,8 @@ import metalist from "../metalist.js"
         <a href="https://c-kikuchi.github.io/iiif/iike-15/manifest.json" target="_blank">Test Manifest</a>
       </div>-->
     </nav>
+    <div>
+      mode: {{ isDev }}
+    </div>
   </main>
 </template>
