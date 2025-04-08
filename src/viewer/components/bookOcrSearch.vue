c697ea99cc1d02bec66059d80d3afe79bbdf8278
chihiro kikuchi <c-kikuchi@hi.u-tokyo.ac.jp>
2025-04-09 00:27:11 +0900

    book ocr search panel (wip) (2)

--- src/viewer/components/bookOcrSearch.vue
+++ src/viewer/components/bookOcrSearch.vue
@@ -22,6 +22,11 @@
     </div>
   </div>
   <div v-else>
+    <div class="control">
+      <input v-model="search_query"><br>
+      <label><input type="checkbox" v-model="only_this_page"><small>このページのみ検索</small></label><br>
+      {{ current_ocrs.length }} Annotations
+    </div>
 
   </div>
 </div>
@@ -35,14 +40,35 @@ const props = defineProps(["bookid", "show_ocr", "currentPage"]);
 const emit = defineEmits(["navigate"]);
 const current_meta = computed(()=>{
   return metalist.find(meta=>meta.bookid==props.bookid);
+});
+const imageUrlRoot = computed(()=>{
+  const meta = current_meta.value;
+  return meta.imageUrl.server+meta.imageUrl.prefix+meta.identifier+meta.imageUrl.suffix
+});
+const getpage_exp = computed(()=>{
+  return new RegExp(`^${imageUrlRoot.value}(.+)${current_meta.value.imageUrl.extension}$`)
 })
 const has_ocr = computed(()=>{
-  return !!current_meta.ocrtext;
+  return !!current_meta.value.ocrtext;
 })
-const current_ocrs = computed(()=>{
+const current_book_ocrs = computed(()=>{
   return annotStore.ocrs.filter(annot=>annot["_bookid"]==props.bookid);
 });
+const current_ocrs = computed(()=>{
+  return only_this_page.value?current_book_ocrs.value.filter(annot=>get_page(annot)==props.currentPage):current_book_ocrs.value;
+})
 
+const only_this_page = ref(false);
 const search_query = ref("");
+const search_results = ref([]);
+
+function get_page(annotation){
+  if(annotation._page){
+    return annotation._page;
+  }
+  else{
+    return getpage_exp.value.test(annotation.target.source)?RegExp.$1:"";
+  }
+}
 
 </script>
