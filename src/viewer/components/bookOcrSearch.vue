d33f9fd7b251a53c71b83385b46a17ca2759765e
chihiro kikuchi <c-kikuchi@hi.u-tokyo.ac.jp>
2025-04-08 22:54:41 +0900

    book ocr search panel (wip)

--- src/viewer/components/bookOcrSearch.vue
+++ src/viewer/components/bookOcrSearch.vue
@@ -1,13 +1,25 @@
 <style scoped>
+.caution-message{
+  background-color: #f99;
+  color:#f00;
+  border:solid 1px #f00;
+  padding:10px;
+  text-align:center;
+  font-weight:bold;
+}
 
 </style>
 <template>
 <div>
   <div v-if="!has_ocr">
-    [!] この巻にはOCRテキストがありません
+    <div class="caution-message">
+      [!] この巻にはOCRテキストがありません
+    </div>
   </div>
   <div v-else-if="!show_ocr">
-    [!] ［OCR結果を表示］にチェックしてください
+    <div class="caution-message">
+      [!] ［OCR結果を表示］にチェックしてください
+    </div>
   </div>
   <div v-else>
 
@@ -19,7 +31,8 @@ import { ref, computed, inject } from "vue";
 import metalist from "../../metalist";
 
 const annotStore = inject("annotStore");
-const props = defineProps(["bookid", "show_ocr"]);
+const props = defineProps(["bookid", "show_ocr", "currentPage"]);
+const emit = defineEmits(["navigate"]);
 const current_meta = computed(()=>{
   return metalist.find(meta=>meta.bookid==props.bookid);
 })
@@ -30,5 +43,6 @@ const current_ocrs = computed(()=>{
   return annotStore.ocrs.filter(annot=>annot["_bookid"]==props.bookid);
 });
 
+const search_query = ref("");
 
 </script>
