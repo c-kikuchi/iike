5a1e54fa12dbc816b8e32d59451a0dd3a361164a
chihiro kikuchi <c-kikuchi@hi.u-tokyo.ac.jp>
2026-07-29 10:20:06 +0900

    add annotation visible toggle

--- src/viewer/index.vue
+++ src/viewer/index.vue
@@ -252,7 +252,7 @@ input[type=checkbox]:checked.togglebutton+span {
         <label role="button" aria-role="button"><input class="togglebutton" type="checkbox" v-model="is_annotating" @change="startAnnotationMode()"><span>⌖索引の作成</span></label>
         <label role="button" aria-role="button"><input class="togglebutton" type="checkbox" v-model="is_taggingmode" @change="startTagAnnotationMode()"><span>文書番号指定</span></label>
         <label role="button" aria-role="button"><input class="togglebutton" type="checkbox" v-model="is_memomode" @change="startMemoAnnotationMode()"><span>メモ作成</span></label>
-        
+        &nbsp;<label style="color:#fff;font-size:small;"><input type="checkbox" checked @change="e=>anno.setVisible(e.target.checked)">タグを表示</label>
         &nbsp;<label style="color:#fff;font-size:small;"><input type="checkbox" v-model="show_ocrs" @change="loadOcr" :disabled="!has_ocr">OCR結果を表示</label>
       </div>
     </div>
